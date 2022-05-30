af59b2d3e2c329cfb3de8c5ee3ea5d9e32d82874
dkchae <dkchae@nhrcomm.com>
2022-05-30 13:49:48 +0900

    주석추가

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 
 const MyApp = ({ Component, pageProps }) => {
+  // Component는 불러오는 페이지, 여기서는 pages/indexjsx를 의미함
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      {/* pageProps는 pages안의 파일에서 getServerSideProps, getStaticProps 혹은 getInitialProps로 페이지에 전달해주는 props다 */}
       <style jsx global>{`
         body {
           margin: 0;
